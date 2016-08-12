58ea05af6e90011b682e77119d8fe6d7c32167b9
hanson <337993334@qq.com>
2016-08-12 15:35:45 +0800

    change code style

--- app/src/main/java/com/funnyshine/hanson/travel/activity/MainActivity.java
+++ app/src/main/java/com/funnyshine/hanson/travel/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.funnyshine.hanson.travel;
+package com.funnyshine.hanson.travel.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.funnyshine.hanson.travel.R;
 
 public class MainActivity extends AppCompatActivity {
 
